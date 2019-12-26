fcef10a8240e2273404384ee83ef646011ed1c32
quynhvi <quynh.nguyen@mservice.com.vn>
2019-12-26 16:50:17 +0700

    rest example rest example

---
+++ src/main/java/com/mservice/dvcwrapper/rest/ConferenceController.java
@@ -0,0 +1,20 @@
+package com.mservice.dvcwrapper.rest;
+
+import com.mservice.dvcwrapper.bean.Conference;
+import com.mservice.dvcwrapper.service.ConferenceService;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("conferences")
+public class ConferenceController {
+    private final ConferenceService conferenceService;
+
+    public ConferenceController(ConferenceService conferenceService) {
+        this.conferenceService = conferenceService;
+    }
+
+    @Get("/random")
+    public Conference randomConf() {
+        return conferenceService.randomConf();
+    }
+}
