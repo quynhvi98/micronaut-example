bfda68aa116042c834357100262b9099e3d91a58
quynhvi <quynh.nguyen@mservice.com.vn>
2019-12-27 13:46:25 +0700

    micronaut security session  example

--- src/main/java/com/mservice/dvcwrapper/rest/AuthController.java
+++ src/main/java/com/mservice/dvcwrapper/rest/AuthController.java
@@ -5,16 +5,32 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.security.annotation.Secured;
+import io.micronaut.views.View;
 
+import javax.annotation.Nullable;
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
-@Secured("isAuthenticated()")
+//@Secured("isAuthenticated()")
+@Secured("isAnonymous()")
 @Controller("/")
 public class AuthController {
 
-    @Produces(MediaType.TEXT_PLAIN)
+//    @Produces(MediaType.TEXT_PLAIN)
+//    @Get("/")
+//    public String index(Principal principal) {
+//        return principal.getName();
+//    }
+
     @Get("/")
-    public String index(Principal principal) {
-        return principal.getName();
+    @View("home")
+    Map<String, Object> index(@Nullable Principal principal) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("loggedIn", principal != null);
+        if (principal != null) {
+            data.put("username", principal.getName());
+        }
+        return data;
     }
 }
